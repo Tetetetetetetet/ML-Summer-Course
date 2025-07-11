a30eea8566c5edcf6069878f2cb8989860a7f104
Tet <tett8107@gmail.com>
2025-07-11 16:27:24 +0800

    solving the missing

--- src/data_missing.py
+++ src/data_missing.py
@@ -22,6 +22,14 @@ class MissingDataHandler:
         self.features_config = self.feature_json['features']  # 获取features字段
         self.train_data = None
         
+    def load_recoded_data(self):
+        """
+        加载recoded后的数据
+        """
+        data_path = os.path.join(self.output_dir, 'recoded_train.csv')
+        if not os.path.exists(data_path):
+            raise FileNotFoundError(f"recoded后的数据文件不存在: {data_path}")
+        self.train_data = pd.read_csv(data_path)
     def load_normalized_data(self):
         """
         加载归一化后的数据
@@ -109,16 +117,19 @@ class MissingDataHandler:
         mid_missing_features = self.feature_json['missing_type']['mid']
         return self.train_data.dropna(subset=mid_missing_features)
     
-    def ppca(self, data: pd.DataFrame):
+    def pca(self, data: pd.DataFrame):
         '''
         对于数据集data做主成分分析
         args:
             - data: pd.DataFrame, 完整的数据集（无缺失值）
         '''
+        exp_name = 'first_try'
         # 创建可视化输出目录
-        vis_dir = self.output_dir / 'ppca_analysis'
+        vis_dir = self.output_dir /'pca_analysis'/f'{exp_name}'
         os.makedirs(vis_dir, exist_ok=True)
         
+        # 去掉label
+        data = data.drop(columns=['readmitted'])
         # 数据标准化
         scaler = StandardScaler()
         scaled_data = scaler.fit_transform(data)
@@ -173,7 +184,7 @@ class MissingDataHandler:
         with open(vis_dir / 'pca_analysis.json', 'w') as f:
             json.dump(analysis_results, f, indent=4)
         
-        logging.info(f"PPCA分析完成，结果保存在: {vis_dir}")
+        logging.info(f"PCA分析完成，结果保存在: {vis_dir}")
         return pca, loadings
 
     def process_low_missing_features(self):
@@ -233,10 +244,11 @@ class MissingDataHandler:
 
 def main():
     handler = MissingDataHandler()
-    handler.load_normalized_data()
+    # handler.load_normalized_data()
+    handler.load_recoded_data()
     handler.analyze_missing_values()
     low_complete_data = handler.process_low_missing_features()
-    handler.ppca(low_complete_data)
+    handler.pca(low_complete_data)
     return
     mid_complete_data = handler.process_mid_missing_features()
     handler.create_complete_datasets()
